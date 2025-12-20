a76cfd43d59cfa936323b55d362be9e27ee3c56f
Edward Ruzek <edadiddles@hotmail.com>
2025-12-20 10:01:38 -0500

    have the program running to completion without deadlock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+
+    // TODO: spawn 3 producer threads
+    for i in 0..3 {
+        let txc = tx.clone();
+        thread::spawn(move || {
+            for n in 0..5 {
+                let k = i*10 + n;
+                txc.send(k).unwrap();
+                println!("Sent {k}");
+            }
+        });
+    }
+
+    // TODO: drop the original sender if needed
+    drop(tx);
+
+    // Consumer
+    for value in rx {
+        println!("received {}", value);
+    }
+
+    println!("all done");
 }
