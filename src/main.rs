5f72af6f37d9c005ce184e09a1a76d1df803dcfe
Edward Ruzek <edadiddles@hotmail.com>
2025-12-20 13:18:12 -0500

    adding vec to track thread handles and joining handles after consumption.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@ use std::thread;
 fn main() {
     let (tx, rx) = mpsc::channel();
 
+    let mut handles = Vec::new();
     // TODO: spawn 3 producer threads
     for i in 0..3 {
         let txc = tx.clone();
-        thread::spawn(move || {
+        handles.push(thread::spawn(move || {
             for n in 0..5 {
                 let k = i*10 + n;
                 txc.send(k).unwrap();
                 println!("Sent {k}");
             }
-        });
+        }));
     }
 
     // TODO: drop the original sender if needed
@@ -22,6 +23,10 @@ fn main() {
     // Consumer
     for value in rx {
         println!("received {}", value);
+    }
+
+    for h in handles {
+        h.join().unwrap()
     }
 
     println!("all done");
