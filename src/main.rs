467e105d8d8d38f562071fe441bfcb45bfbd47d3
Edward Ruzek <edadiddles@hotmail.com>
2025-12-21 08:08:17 -0500

    added hash map to track sent data by producer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::sync::mpsc;
 use std::thread;
+use std::collections::hash_map::HashMap;
 
 enum Msg {
-    Data(i32),
-    Done,
+    Data(i32, i32),
+    Done(i32),
 }
 
 fn main() {
@@ -16,10 +17,10 @@ fn main() {
         handles.push(thread::spawn(move || {
             for n in 0..5 {
                 let k = i*10 + n;
-                txc.send(Msg::Data(k)).unwrap();
+                txc.send(Msg::Data(i, k)).unwrap();
                 println!("Sent {k}");
             }
-            txc.send(Msg::Done).unwrap();
+            txc.send(Msg::Done(i)).unwrap();
         }));
     }
 
@@ -27,10 +28,18 @@ fn main() {
     drop(tx);
 
     // Consumer
+    let mut map: HashMap<i32, i32> = HashMap::new();
     for value in rx {
         match value {
-            Msg::Data(x) => println!("received {x}"),
-            Msg::Done => println!("producer finished"),
+            Msg::Data(x, y) => {
+                println!("received {y} from producer {x}");
+                let k = map.get(&x).unwrap_or(&0);
+                map.insert(x, y+k);
+            },
+            Msg::Done(x) => {
+                println!("producer {x} finished");
+                println!("producer {x} sent sum {}", map.get(&x).unwrap_or(&0));
+            },
         }
         
     }
